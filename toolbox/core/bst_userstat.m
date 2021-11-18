a20edcb385739ed996d15632086c027db8998561
jesusmartin-cortecero <53047456+jesusmartin-cortecero@users.noreply.github.com>
2021-11-18 17:11:30 +0100

    Merge branch 'master' of https://github.com/brainstorm-tools/brainstorm3

--- toolbox/core/bst_userstat.m
+++ toolbox/core/bst_userstat.m
@@ -36,16 +36,10 @@ hf = 230;
 hFig = [];
 % Output folder for images
 ImgDir = 'C:\Work\Doc\Brainstorm\site\stat';
-% Reading function: urlread replaced with webread in Matlab 2014b
-if (bst_get('MatlabVersion') <= 803)
-    url_read_fcn = @urlread;
-else
-    url_read_fcn = @webread;
-end
 
 % ===== NUMBER OF USERS =====
 % Read list of users
-str = url_read_fcn('http://neuroimage.usc.edu/bst/get_userdate.php?c=k9w8cX');
+str = bst_webread('http://neuroimage.usc.edu/bst/get_userdate.php?c=k9w8cX');
 % Extract values
 dates = textscan(str, '%d %d');
 dates = double([dates{1}, dates{2}]);
@@ -62,7 +56,7 @@ hFig(end+1) = fig_report(year, nUsersTotal, 0, ...
        
 % ===== LOG ANALYSIS =====
 % Read list of users
-str = url_read_fcn('http://neuroimage.usc.edu/bst/get_logs.php?c=J7rTwq');
+str = bst_webread('http://neuroimage.usc.edu/bst/get_logs.php?c=J7rTwq');
 % Extract values
 c = textscan(str, '%02d%02d%c');
 dates = double([c{1}, c{2}]);
@@ -95,7 +89,7 @@ nUpdate(iBad) = interp1(xUpdate(~iBad), nUpdate(~iBad), xUpdate(iBad), 'pchip');
 
 % ===== NUMBER OF FORUM POSTS =====
 % Read list of users
-str = url_read_fcn('http://neuroimage.usc.edu/bst/get_posts.php?c=3Emzpjt0');
+str = bst_webread('http://neuroimage.usc.edu/bst/get_posts.php?c=3Emzpjt0');
 % Extract values
 dates = textscan(str, '%d %d');
 dates = double([dates{1}, dates{2}]);
@@ -112,7 +106,7 @@ hFig(end+1) = fig_report(year(1:end-1), nPosts(1:end-1), 0, ...
 % Hard coded list of publications
 year   = [2000 2001 2002 2003 2004 2005 2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019]; 
 nPubli = [   2    2    1    1    3    5    5   11   10   20   20   32   38   55   78   94  133  214  225  300];
-nPubliCurYear = 177;
+nPubliCurYear = 234;
 % Plot figure
 hFig(end+1) = fig_report(year, nPubli, 1, ...
            [2000 max(year)], [], ...
